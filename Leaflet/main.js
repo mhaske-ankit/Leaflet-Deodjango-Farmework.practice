009e21796b48d8e43029cb9165eec88ab2f9edbd
mhaske-ankit <140797841+mhaske-ankit@users.noreply.github.com>
2023-09-09 12:04:04 +0530

    Yesterday I have start to learn new Django framework

---
+++ Leaflet/main.js
@@ -0,0 +1,15 @@
+window.onload = init;
+
+function init(){
+      
+      const mymap = L.mymap('mapid',{
+            center: [48,14],
+            zoom: 4,
+            layers:[
+                  L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',{
+                        attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+                  })
+            ]
+
+      })
+}
